0549de37ecc3aff01e5d8c0ff563b0167404edf1
upz17 <57347370+upz17@users.noreply.github.com>
2020-02-04 01:00:43 +0700

    test

--- app/api/server/default/info.js
+++ app/api/server/default/info.js
@@ -1,3 +1,23 @@
 import { hasRole } from '../../../authorization';
 import { Info } from '../../../utils';
 import { API } from '../api';
+
+API.default.addRoute('info', { authRequired: false }, {
+	get() {
+		const user = this.getLoggedInUser();
+
+		if (user && hasRole(user._id, 'admin')) {
+			return API.v1.success({
+				info: Info,
+				info2: Info,
+				info3: Info,
+			});
+		}
+
+		return API.v1.success({
+			version: Info.version,
+			version2: Info.version,
+			version3: Info.version,
+		});
+	},
+});
